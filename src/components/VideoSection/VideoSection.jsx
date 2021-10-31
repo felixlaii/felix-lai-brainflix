105b4c63b3a29b638687bd627651b0d11008e3a2
felixlaii <felix.lai@hotmail.com>
2021-10-31 16:11:13 -0400

    added styles to metrics

--- src/components/VideoSection/VideoSection.jsx
+++ src/components/VideoSection/VideoSection.jsx
@@ -3,9 +3,18 @@ import PlayIcon from '../../assets/icons/play.svg';
 import ScrubControl from '../../assets/icons/scrub.svg';
 import FullScreenIcon from '../../assets/icons/fullscren.svg';
 import VolumeUp from '../../assets/icons/volume_up.svg';
+import VideoViews from '../../assets/icons/views.svg';
+import VideoLikes from '../../assets/icons/likes.svg';
 
 function VideoSection(props) {
     const { title, channel, image, description, views, likes, duration, timestamp } = props.selectedVideo;
+
+    let postedDate = new Date(timestamp);
+
+    postedDate = postedDate.toLocaleString().slice(0,9);
+    postedDate = postedDate.replace(/ ,/g, '')
+    postedDate = postedDate.trim();
+    console.log(postedDate)
 
     return (
         <div className="primary-video">
@@ -25,14 +34,21 @@ function VideoSection(props) {
             <h2 className="primary-video__title">{title}</h2>
 
             <div className="primary-video__metrics">
-                <p className="primary-video__metrics-channel">{channel}</p>
-                <p className="primary-video__metrics-views">{views}</p>
-            </div>
-                <div className="primary-video__info">
-                    <p className="primary-video__info-date">{timestamp}</p>
-                    <p className="primary-video__info-likes">{likes}</p>
+                <div className="primary-video__container">
+                    <p className="primary-video__container-channel"><strong>By {channel}</strong></p>
+                    <p className="primary-video__container-date">{postedDate}</p>
                 </div>
-            
+                <div className="primary-video__info">
+                    <div className="primary-video__reachability">
+                        <img className="primary-video__viewsicon" src={VideoViews} alt='Video Views' />
+                        <p className="primary-video__info-views">{views}</p>
+                    </div>
+                    <div className="primary-video__likeability">
+                        <img className="primary-video__likesicon" src={VideoLikes} alt='Video Likes' />
+                        <p className="primary-video__info-likes">{likes}</p>
+                    </div>
+                </div>
+            </div>
             <p className="primary-video__description">{description}</p>
             
         </div>
