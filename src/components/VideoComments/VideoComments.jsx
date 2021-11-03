1c7bafaf0dbc40639edc1838d3f00dec9a6ded2a
felixlaii <felix.lai@hotmail.com>
2021-11-03 19:17:09 -0400

    updated icons and active and hover states

--- src/components/VideoComments/VideoComments.jsx
+++ src/components/VideoComments/VideoComments.jsx
@@ -1,7 +1,6 @@
 import './videocomments.scss';
 
 import SearchAva from '../../assets/images/Mohan-muruge.jpg';
-import AddComment from '../../assets/icons/add_comment.svg';
 
 function VideoComments(props) {
    const { name, comment, timestamp } = props.selectedVideo;
@@ -10,13 +9,12 @@ function VideoComments(props) {
         <div className="video-comments">
             <p className="video-comments__counter">3 Comments</p>
             <img className="video-comments__avatar" src={SearchAva} alt='comment form avatar' />
-            <img className="video-comments__addcomment" src={AddComment} alt='comment submit icon' />
            
                 <label className="video-comments__label">JOIN THE CONVERSATION</label>
                 <div className="video-comments__form">
                 <input className="video-comments__input"type="text" name="name" placeholder="Add a new comment"></input>
                 <input className= "video-comments__submit" type="submit" value="COMMENT"></input>
-            </div>
+                </div>
         </div>
     )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,5 @@
 import BrainFlixLogo from '../../assets/logo/BrainFlix-logo.svg';
 import UploadIcon from '../../assets/icons/upload.svg';
-// import SearchIcon from '../../assets/icons/search.svg';
-// import SearchAva from '../../assets/images/Mohan-muruge.jpg';
 import './Header.scss';
 
 const Header = () => {
@@ -9,11 +7,7 @@ const Header = () => {
     <nav className="primary-nav">
         <img className="primary-nav__logo" src={BrainFlixLogo} alt="primary nav logo" />
         <div className="primary-nav__searchbar">
-            {/* <div className="primary-nav__searchbar--avatar"></div> */}
-            {/* <img className="primary-nav__searchbar--avatar" src={SearchAva} alt="search bar avatar" /> */}
-            {/* <img className="primary-nav__searchbar--icon" src={SearchIcon} alt="search icon" /> */}
             <input className="primary-nav__search primary-nav__search--avatar" type="text" name="search" placeholder="Search" />
-            {/* <img className="primary-nav__upload--icon" src={UploadIcon} alt="primary icon" /> */}
             <input className= "primary-nav__upload" type="submit" value="UPLOAD"></input>
         </div>
 
