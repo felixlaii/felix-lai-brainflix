627d89933356314b09fc9f969dd5c1e8c438c6bd
felixlaii <felix.lai@hotmail.com>
2021-11-03 20:03:57 -0400

    added avatar to search bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,18 @@
 import BrainFlixLogo from '../../assets/logo/BrainFlix-logo.svg';
-import UploadIcon from '../../assets/icons/upload.svg';
+import SearchAva from '../../assets/images/Mohan-muruge.jpg';
 import './Header.scss';
 
 const Header = () => {
     return (
     <nav className="primary-nav">
         <img className="primary-nav__logo" src={BrainFlixLogo} alt="primary nav logo" />
-        <div className="primary-nav__searchbar">
-            <input className="primary-nav__search primary-nav__search--avatar" type="text" name="search" placeholder="Search" />
+        {/* use to be searchbar */}
+        <div className="primary-nav__searchandupload"> 
+            <div className="primary-nav__searchbar">
+                <input className="primary-nav__search" type="text" name="search" placeholder="Search" />
+                <img className="primary-nav__search--avatar" src={SearchAva} alt="Search Bar Avatar" />
+                {/* <div className="primary-nav__search--avatar"></div> */}
+            </div>
             <input className= "primary-nav__upload" type="submit" value="UPLOAD"></input>
         </div>
 
